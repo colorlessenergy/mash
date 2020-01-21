68c8a4c659d280233ce2647a61ee1b9e592bd51d
colorlessenergy <brianmunozdev@gmail.com>
2020-01-21 08:17:14 -0800

    create and read a post in the backend

--- app.js
+++ app.js
@@ -1,6 +1,27 @@
 const express = require('express');
 const app = express();
 
+const mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
+
+mongoose.connect('mongodb://mash:mash123@ds253857.mlab.com:53857/mash', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then(() => console.log('connected to DB'))
+  .catch(err => console.log('there was an error ' + err));
+
+const bodyParser = require('body-parser');
+
+// dump json in body into req.body
+
+app.use(bodyParser.json());
+
+
+const postRouter = require('./routes/PostRouter');
+
+app.use('/posts', postRouter);
+
 app.listen(process.env.PORT || 3001, function () {
   console.log('backend server is running')
 });
